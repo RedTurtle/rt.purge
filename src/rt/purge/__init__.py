9554c2f6552b769df138dc542530f45c0811cf55
keul <luca@keul.it>
2012-09-04 08:26:05 +0000

    mancavano dei file non committati

--- src/rt/purge/__init__.py
+++ src/rt/purge/__init__.py
@@ -6,6 +6,3 @@ from zope.i18nmessageid import MessageFactory
 
 purgerMessageFactory =  MessageFactory('rt.purge')
 logger = logging.getLogger('rt.purge')
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
